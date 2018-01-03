9048a4a090591cfbd9e118a9ce61435a810c5292
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-03 12:01:28 +0700

    java

---
+++ Operator.java
@@ -0,0 +1,22 @@
+public class Operator{
+	public static void main(String[] args){
+
+     System.out.println("Pejulahan");
+
+     int a =10;
+     int b =11;
+     System.out.println("Jumlah 10 + 11 = "+(a+b));
+
+     int c =10;
+     int d =11;
+     System.out.println("Jumlah 10 + 11 = "+(c-d));
+
+     int e =10;
+     int f =11;
+     System.out.println("Jumlah 10 + 11 = "+(e*f));
+
+
+      int tes=10-20;
+      System.out.println("Jumlahnya adalah = "+(tes == -10));
+	}
+}
